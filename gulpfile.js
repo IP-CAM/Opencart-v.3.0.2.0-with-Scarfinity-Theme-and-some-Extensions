c9d707cca09848a4e473afc438aadad9962f78c3
Михаил <VitAm1nKa@hotmail.com>
2018-12-07 02:15:29 +0700

    First commit

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+var     
+    gulp            = require('gulp'),
+    less            = require('gulp-less'),
+    concatCss       = require('gulp-concat-css'),
+    cleanCss        = require('gulp-clean-css'),
+    autoprefixer    = require('gulp-autoprefixer'),
+    browserSync     = require('browser-sync'),
+    bourbon         = require('node-bourbon'),
+    ftp             = require('vinyl-ftp'),
+    path            = require('path');
+
+gulp.task('browser-sync', function() {
+    browserSync({
+        proxy: 'scarfinity.loc:170',
+        notify: false
+    });
+});
+
+gulp.task('less', function() {
+    console.log(bourbon.includePaths);
+    return gulp.src('catalog/view/theme/scarfinity/stylesheet/**/*.less')
+        .pipe(less({ paths: bourbon.includePaths }))
+        .pipe(autoprefixer(['last 15 versions']))
+        .pipe(concatCss('stylesheet.css'))
+        .pipe(cleanCss())
+        .pipe(gulp.dest('catalog/view/theme/scarfinity/stylesheet'))
+        .pipe(browserSync.reload({stream: true}));
+});
+
+gulp.task('watch', ['less', 'browser-sync'], function() {
+    gulp.watch('catalog/view/theme/scarfinity/stylesheet/**/*.less', ['less']);
+    gulp.watch('catalog/view/theme/scarfinity/template/**/*.twig', browserSync.reload);
+    gulp.watch('catalog/view/theme/scarfinity/js/**/*.js', browserSync.reload);
+    gulp.watch('catalog/view/theme/scarfinity/libs/**/*', browserSync.reload);
+});
+
+gulp.task('default', ['watch']);
