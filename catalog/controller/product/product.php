f47e2d6a58462088c133d0db2e9df184e428f8f6
Михаил <VitAm1nKa@hotmail.com>
2019-02-04 23:55:21 +0700

    Опции товара

--- catalog/controller/product/product.php
+++ catalog/controller/product/product.php
@@ -335,8 +335,8 @@ class ControllerProductProduct extends Controller {
 							'product_option_value_id' => $option_value['product_option_value_id'],
 							'option_value_id'         => $option_value['option_value_id'],
 							'name'                    => $option_value['name'],
-							'image'                   => $this->model_tool_image->resize($option_value['image'], 50, 50),
-							'image_f'				  => $this->model_tool_image->resize($option_value['image'], 250, 250),
+							'image'                   => $this->model_tool_image->resize($option_value['image'], 150, 150),
+							'image_f'				  => $this->model_tool_image->resize($option_value['image'], 1024, 1024),
 							'price'                   => $price,
 							'price_prefix'            => $option_value['price_prefix']
 						);
